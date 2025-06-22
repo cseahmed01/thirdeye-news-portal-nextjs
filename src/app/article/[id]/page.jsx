b16fad0a0cd24819f1ef5b4bc36ff85fcee13dea
Nafis Nihal <nafisnihal12@gmail.com>
2025-06-22 17:29:06 +0600

    refactor: remove unused generateStaticParams function from article page

--- src/app/article/[id]/page.jsx
+++ src/app/article/[id]/page.jsx
@@ -2,13 +2,6 @@ import Container from "@/components/common/Container";
 import ArticleBody from "@/components/pages/article/ArticleBody";
 import { fetchData } from "@/lib/fetchData";
 
-// export async function generateStaticParams() {
-//   const articles = await fetchData("articles"); // Fetch all articles
-//   return articles.map((article) => ({
-//     id: article.id.toString(), // Ensure it's a string
-//   }));
-// }
-
 export default async function page({ params }) {
   const { id } = await params;
 
